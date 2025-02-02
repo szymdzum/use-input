e0798c37e32970bb6cfa48964e3404a6a51b3f69
Kumak <szymon@kumak.dev>
2025-02-02 06:21:59 +0100

    footer navbar visual

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -1,14 +1,28 @@
 import { NavLink } from "react-router";
 import styles from "./NavBar.module.css";
+
+const getNavLinkClass = ({ isActive }: { isActive: boolean }) =>
+  isActive ? `${styles.link} ${styles.activeLink}` : styles.link;
+
 function NavBar() {
   return (
-      <footer className={styles.footer}>
-      <h1 title="useInput">useInput</h1>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/register">Register</NavLink>
-        <NavLink to="/test">Test</NavLink>
-        <NavLink to="/login">Login</NavLink>
+    <footer className={styles.footer}>
+      <h1 title="useInput" className={styles.title}>
+        useInput
+      </h1>
+      <nav className={styles.nav}>
+        <NavLink to="/" className={getNavLinkClass}>
+          Home
+        </NavLink>
+        <NavLink to="/register" className={getNavLinkClass}>
+          Register
+        </NavLink>
+        <NavLink to="/test" className={getNavLinkClass}>
+          Test
+        </NavLink>
+        <NavLink to="/login" className={getNavLinkClass}>
+          Login
+        </NavLink>
       </nav>
     </footer>
   );
